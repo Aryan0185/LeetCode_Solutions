057732751cf408db9ed588700bb7c3e91acabf98
Aryan <aryansaini42817@gmail.com>
2026-06-04 18:53:09 +0530

    Time: 1 ms (25.35%), Space: 71.8 MB (14.82%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int maxlen=INT_MIN,sum=0;
+        for(int i=0;i<n;i++){
+            if(sum<0)sum=0;
+             sum=sum+nums[i];
+            maxlen=max(maxlen,sum);
+        } return maxlen;
+    }
+};
