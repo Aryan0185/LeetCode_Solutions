d4534fc43b598536e62a44e9d77bb681de669cfe
Aryan <aryansaini42817@gmail.com>
2026-06-21 19:27:43 +0530

    Time: 13 ms (31.66%), Space: 7.8 MB (37.72%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-     int fact(int nums){
-        if(nums==1 || nums==2)return 1;
-        return fact(nums-1)+fact(nums-2);
-     }
     int fib(int n) {
         if(n==0)return 0;
-        return  fact(n);
+        if(n==1 || n==2)return 1;
+        return  fib(n-1)+fib(n-2);
       }
 };
