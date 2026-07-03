59f5423bd42fe6ee45dad62ac960ef04ad31ba05
Aryan <aryansaini42817@gmail.com>
2026-07-03 16:35:17 +0530

    Time: 0 ms (100%), Space: 9.2 MB (55.51%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-        int i=0,n=s.size();
-        while(i<n && s[i]==' ')i++;
-        int sign=1;
-        if(i<n &&( s[i]=='+'|| s[i]=='-')){
-            if(s[i]=='-') sign=-1;
+    void skipspace(string & s,int & i,int n){
+        if(i<n && s[i]==' '){
             i++;
+            skipspace(s,i,n);
         }
-        long result=0;
-        while(i<n && isdigit(s[i])){
-         result=result*10+(s[i]-'0');
-         if(result > INT_MAX) {         
-        return (sign==1) ? INT_MAX : INT_MIN;
     }
+    long readdigits(string & s,int & i,int n,long result){
+        if(i>=n || !isdigit(s[i])) return result;
+         result = result*10 + (s[i]-'0');
+        if(result > INT_MAX) return result; 
+        i++;
+        return readdigits(s,i,n,result);
+    }
+    int myAtoi(string s) {
+       int i=0;
+       int n=s.size();
+       skipspace(s,i,n); 
+       int sign=1;
+       if(i<n && (s[i]=='+'|| s[i]=='-')){
+         if(s[i]=='-')sign=-1;
          i++;
-        }
+       }
+       long result=readdigits(s,i,n,0);
+       if(result > INT_MAX) return (sign==1) ? INT_MAX : INT_MIN;
         return (int)(result*sign);
     }
 };
