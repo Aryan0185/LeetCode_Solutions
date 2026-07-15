c31ac2afc6191d298de4d4f1df1e494d99b4ecef
Aryan <aryansaini42817@gmail.com>
2026-07-15 12:05:15 +0530

    Time: 3 ms (73.59%), Space: 15.7 MB (57.67%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,15 +1,45 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        long long ans=0;
-        for(int i=0;i<nums.size();i++){
-            int mx=nums[i],mn=nums[i];
-            for(int j=i;j<nums.size();j++){
-                  mx=max(mx,nums[j]);
-                  mn=min(mn,nums[j]);
-                  ans+=mx-mn;
-            }
+        int n = nums.size();
+        vector<int> minLeft(n), minRight(n), maxLeft(n), maxRight(n);
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] >= nums[i]) st.pop();
+            minLeft[i] = st.empty() ? -1 : st.top();
+            st.push(i);
         }
-        return ans;
+        while (!st.empty()) st.pop();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] > nums[i]) st.pop();
+            minRight[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] <= nums[i]) st.pop();
+            maxLeft[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] < nums[i]) st.pop();
+            maxRight[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        long long minSum = 0, maxSum = 0;
+        for (int i = 0; i < n; i++) {
+            long long minCount = (long long)(i - minLeft[i]) * (minRight[i] - i);
+            long long maxCount = (long long)(i - maxLeft[i]) * (maxRight[i] - i);
+            minSum += minCount * nums[i];
+            maxSum += maxCount * nums[i];
+        }
+
+        return maxSum - minSum;
     }
 };
