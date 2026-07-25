ec5036bafc40492b41a66686970591c915d0460e
Aryan <aryansaini42817@gmail.com>
2026-07-25 20:29:19 +0530

    Time: 0 ms (100%), Space: 21.8 MB (39.09%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int count=0;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(count==nums[i])count++;
-            else return count;
+        int low=0,high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==mid)
+            low=mid+1;
+            else high=mid-1;
         }
-        return count;
+        return low;
     }
 };
