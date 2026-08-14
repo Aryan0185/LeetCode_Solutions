4e5ae347de94dc81aea0edc37b43e8d29f3d3cae
Aryan <aryansaini42817@gmail.com>
2026-08-14 16:37:00 +0530

    Time: 0 ms (100%), Space: 8 MB (60.71%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -3,9 +3,13 @@ public:
     int minBitFlips(int start, int goal) {
         int ans=start^goal;
         int count=0;
-       for(int i=0;i<=31;i++){
+        while(ans!=0){
+            count++;
+            ans=(ans&(ans-1));
+        }
+      /* for(int i=0;i<=31;i++){
         if(ans&(1<<i)) count++;
-       }
+       }*/
        return count;
     }
 };
